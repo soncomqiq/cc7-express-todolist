c851410cb43a7b8348bc90eb42a74b0ddc9b60f7
codecamp.official.thailand <codecamp.official.thailand@gmail.com>
2020-10-21 09:48:34 +0700

    step 2

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 const studentRoute = require("./routes/students");
+const todoRoute = require("./routes/todos");
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use("/students", studentRoute);
+app.use("/todos", todoRoute);
 
 app.listen(5555, () => {
   console.log("Server is running at PORT 5555");
